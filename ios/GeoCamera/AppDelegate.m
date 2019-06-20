2c9933fc3fb8e516f76508c806d63c153f715084
Ivan Aguas <aguas.xavier2013a@gmail.com>
2019-06-20 13:26:03 -0500

    iOS app Ready!

--- ios/GeoCamera/AppDelegate.m
+++ ios/GeoCamera/AppDelegate.m
@@ -10,11 +10,15 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyB_yjKhG9m8y1DtBDdCok4FgfvNDX3e3I4"];
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"GeoCamera"
